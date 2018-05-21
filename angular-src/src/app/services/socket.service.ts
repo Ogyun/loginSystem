3d8082e336fe843ad88bd87e67542f50150d8a35
anrskp <Anrskp@gmail.com>
2018-05-21 13:15:48 +0200

    qf

--- angular-src/src/app/services/socket.service.ts
+++ angular-src/src/app/services/socket.service.ts
@@ -4,7 +4,7 @@ import * as socketIo from 'socket.io-client';
 
 export class SocketService {
   token = localStorage.getItem('id_token');
-  private socket = socketIo('http://167.99.246.26:3000', {
+  private socket = socketIo('https://167.99.246.26:3000', {
     query: {token: this.token}
   });
 
