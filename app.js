40c8a39e62d55761121fbdd5f35413a5f4fa4145
anrskp <Anrskp@gmail.com>
2018-06-26 20:49:46 +0200

    cors

--- app.js
+++ app.js
@@ -62,6 +62,14 @@ app.use(express.static('public'));
 // CORS Middleware
 app.use(cors());
 
+app.use(function(req, res, next) {
+//set headers to allow cross origin request.
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 //Body Parser Middleware
 app.use(bodyParser.json());
 
